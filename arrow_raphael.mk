a0718fe9fc2e55309ee56d6f6952caa2cc747311
kubersharma001 <kubersharma001@gmail.com>
2021-10-01 14:33:04 +0000

    raphael: Set build fp,desc at build time
    Change-Id: I1674d68a9b9b8611d75212a2d2dfeb5c3f9df3a9

--- arrow_raphael.mk
+++ arrow_raphael.mk
@@ -29,4 +29,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.2.0.RFKMIXM:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
